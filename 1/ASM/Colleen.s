0f2aa50d27cd0c3f346043364a39e1290ecb2e2c
ZineLeCrack <rlebaillif27@gmail.com>
2025-10-15 16:25:07 +0200

    asm comments

--- 1/ASM/Colleen.s
+++ 1/ASM/Colleen.s
@@ -18,6 +18,7 @@ main:
 	mov rsi, str
 	mov rdi, 1
 
+; --- main loop ---
 	.loop:
 		cmp byte [rsi], 0
 		je .end
@@ -33,7 +34,7 @@ main:
 		cmp byte [rsi], 113
 		je .quote
 		.change
-			mov byte [rsi], bl
+			mov byte [rsi], bl ; --- set char ---
 		.write:
 			mov rax, 1
 			syscall
@@ -69,8 +70,8 @@ main:
 
 section .data
 
-str: db "section .text\nglobal main\n\n; --- ft_putstr ---\n\nft_putstr:\n\tmov rax, 1\n\tmov rsi, rdi\n\tmov rdi, 1\n\tmov rdx, len - 1\n\tsyscall\n\tret\n\n; --- main ---\n\nmain:\n\tmov rdx, 1\n\tmov rsi, str\n\tmov rdi, 1\n\n\t.loop:\n\t\tcmp byte [rsi], 0\n\t\tje .end\n\t\tcmp byte [rsi], 92\n\t\tjne .write\n\t\tinc rsi\n\t\tcmp byte [rsi], 110\n\t\tje .newline\n\t\tcmp byte [rsi], 116\n\t\tje .tab\n\t\tcmp byte [rsi], 115\n\t\tje .str\n\t\tcmp byte [rsi], 113\n\t\tje .quote\n\t\t.change\n\t\t\tmov byte [rsi], bl\n\t\t.write:\n\t\t\tmov rax, 1\n\t\t\tsyscall\n\t\t\tinc rsi\n\t\t\tjmp .loop\n\n\t.newline:\n\t\tmov bl, 10\n\t\tjmp .change\n\n\t.tab:\n\t\tmov bl, 9\n\t\tjmp .change\n\n\t.str:\n\t\tmov rbx, rsi\n\t\tmov rdi, copy\n\t\tcall ft_putstr\n\t\tmov rdx, 1\n\t\tmov rsi, rbx\n\t\tinc rsi\n\t\tjmp .loop\n\n\t.quote:\n\t\tmov bl, 34\n\t\tjmp .change\n\n\t.end:\n\t\txor rax, rax\n\t\tret\n\n; --- data ---\n\nsection .data\n\nstr: db \q\s\q, 0\ncopy: db \q\s\q, 0\nlen: equ $ - copy\n\nsection .note.GNU-stack noalloc noexec nowrite align=1\n", 0
-copy: db "section .text\nglobal main\n\n; --- ft_putstr ---\n\nft_putstr:\n\tmov rax, 1\n\tmov rsi, rdi\n\tmov rdi, 1\n\tmov rdx, len - 1\n\tsyscall\n\tret\n\n; --- main ---\n\nmain:\n\tmov rdx, 1\n\tmov rsi, str\n\tmov rdi, 1\n\n\t.loop:\n\t\tcmp byte [rsi], 0\n\t\tje .end\n\t\tcmp byte [rsi], 92\n\t\tjne .write\n\t\tinc rsi\n\t\tcmp byte [rsi], 110\n\t\tje .newline\n\t\tcmp byte [rsi], 116\n\t\tje .tab\n\t\tcmp byte [rsi], 115\n\t\tje .str\n\t\tcmp byte [rsi], 113\n\t\tje .quote\n\t\t.change\n\t\t\tmov byte [rsi], bl\n\t\t.write:\n\t\t\tmov rax, 1\n\t\t\tsyscall\n\t\t\tinc rsi\n\t\t\tjmp .loop\n\n\t.newline:\n\t\tmov bl, 10\n\t\tjmp .change\n\n\t.tab:\n\t\tmov bl, 9\n\t\tjmp .change\n\n\t.str:\n\t\tmov rbx, rsi\n\t\tmov rdi, copy\n\t\tcall ft_putstr\n\t\tmov rdx, 1\n\t\tmov rsi, rbx\n\t\tinc rsi\n\t\tjmp .loop\n\n\t.quote:\n\t\tmov bl, 34\n\t\tjmp .change\n\n\t.end:\n\t\txor rax, rax\n\t\tret\n\n; --- data ---\n\nsection .data\n\nstr: db \q\s\q, 0\ncopy: db \q\s\q, 0\nlen: equ $ - copy\n\nsection .note.GNU-stack noalloc noexec nowrite align=1\n", 0
+str: db "section .text\nglobal main\n\n; --- ft_putstr ---\n\nft_putstr:\n\tmov rax, 1\n\tmov rsi, rdi\n\tmov rdi, 1\n\tmov rdx, len - 1\n\tsyscall\n\tret\n\n; --- main ---\n\nmain:\n\tmov rdx, 1\n\tmov rsi, str\n\tmov rdi, 1\n\n; --- main loop ---\n\t.loop:\n\t\tcmp byte [rsi], 0\n\t\tje .end\n\t\tcmp byte [rsi], 92\n\t\tjne .write\n\t\tinc rsi\n\t\tcmp byte [rsi], 110\n\t\tje .newline\n\t\tcmp byte [rsi], 116\n\t\tje .tab\n\t\tcmp byte [rsi], 115\n\t\tje .str\n\t\tcmp byte [rsi], 113\n\t\tje .quote\n\t\t.change\n\t\t\tmov byte [rsi], bl ; --- set char ---\n\t\t.write:\n\t\t\tmov rax, 1\n\t\t\tsyscall\n\t\t\tinc rsi\n\t\t\tjmp .loop\n\n\t.newline:\n\t\tmov bl, 10\n\t\tjmp .change\n\n\t.tab:\n\t\tmov bl, 9\n\t\tjmp .change\n\n\t.str:\n\t\tmov rbx, rsi\n\t\tmov rdi, copy\n\t\tcall ft_putstr\n\t\tmov rdx, 1\n\t\tmov rsi, rbx\n\t\tinc rsi\n\t\tjmp .loop\n\n\t.quote:\n\t\tmov bl, 34\n\t\tjmp .change\n\n\t.end:\n\t\txor rax, rax\n\t\tret\n\n; --- data ---\n\nsection .data\n\nstr: db \q\s\q, 0\ncopy: db \q\s\q, 0\nlen: equ $ - copy\n\nsection .note.GNU-stack noalloc noexec nowrite align=1\n", 0
+copy: db "section .text\nglobal main\n\n; --- ft_putstr ---\n\nft_putstr:\n\tmov rax, 1\n\tmov rsi, rdi\n\tmov rdi, 1\n\tmov rdx, len - 1\n\tsyscall\n\tret\n\n; --- main ---\n\nmain:\n\tmov rdx, 1\n\tmov rsi, str\n\tmov rdi, 1\n\n; --- main loop ---\n\t.loop:\n\t\tcmp byte [rsi], 0\n\t\tje .end\n\t\tcmp byte [rsi], 92\n\t\tjne .write\n\t\tinc rsi\n\t\tcmp byte [rsi], 110\n\t\tje .newline\n\t\tcmp byte [rsi], 116\n\t\tje .tab\n\t\tcmp byte [rsi], 115\n\t\tje .str\n\t\tcmp byte [rsi], 113\n\t\tje .quote\n\t\t.change\n\t\t\tmov byte [rsi], bl ; --- set char ---\n\t\t.write:\n\t\t\tmov rax, 1\n\t\t\tsyscall\n\t\t\tinc rsi\n\t\t\tjmp .loop\n\n\t.newline:\n\t\tmov bl, 10\n\t\tjmp .change\n\n\t.tab:\n\t\tmov bl, 9\n\t\tjmp .change\n\n\t.str:\n\t\tmov rbx, rsi\n\t\tmov rdi, copy\n\t\tcall ft_putstr\n\t\tmov rdx, 1\n\t\tmov rsi, rbx\n\t\tinc rsi\n\t\tjmp .loop\n\n\t.quote:\n\t\tmov bl, 34\n\t\tjmp .change\n\n\t.end:\n\t\txor rax, rax\n\t\tret\n\n; --- data ---\n\nsection .data\n\nstr: db \q\s\q, 0\ncopy: db \q\s\q, 0\nlen: equ $ - copy\n\nsection .note.GNU-stack noalloc noexec nowrite align=1\n", 0
 len: equ $ - copy
 
 section .note.GNU-stack noalloc noexec nowrite align=1
