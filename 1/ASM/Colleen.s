8b955f38c420cb5fb94bfb5e0a776507c78a0963
ZineLeCrack <rlebaillif27@gmail.com>
2025-10-15 15:31:44 +0200

    ex 1 asm

--- 1/ASM/Colleen.s
+++ 1/ASM/Colleen.s
@@ -1,36 +1,76 @@
 section .text
-global ft_strlen
 global main
 
-; --- ft_strlen() ---
+; --- ft_putstr ---
 
-ft_strlen:
-	xor rax, rax
-	.loop:
-		cmp byte [rdi], 0
-		je .end
-		inc rdi
-		inc rax
-		jmp .loop
-	.end:
-		ret
+ft_putstr:
+	mov rax, 1
+	mov rsi, rdi
+	mov rdi, 1
+	mov rdx, len - 1
+	syscall
+	ret
 
-; --- main() ---
+; --- main ---
 
 main:
-	mov rdi, str
-	call ft_strlen
-	mov rdi, 1
+	mov rdx, 1
 	mov rsi, str
-	mov edx, eax
-	mov rax, 1
-	syscall
-	xor rax, rax
-	ret
+	mov rdi, 1
+
+	.loop:
+		cmp byte [rsi], 0
+		je .end
+		cmp byte [rsi], 92
+		jne .write
+		inc rsi
+		cmp byte [rsi], 110
+		je .newline
+		cmp byte [rsi], 116
+		je .tab
+		cmp byte [rsi], 115
+		je .str
+		cmp byte [rsi], 113
+		je .quote
+		.change
+			mov byte [rsi], bl
+		.write:
+			mov rax, 1
+			syscall
+			inc rsi
+			jmp .loop
+
+	.newline:
+		mov bl, 10
+		jmp .change
+
+	.tab:
+		mov bl, 9
+		jmp .change
+
+	.str:
+		mov rbx, rsi
+		mov rdi, copy
+		call ft_putstr
+		mov rdx, 1
+		mov rsi, rbx
+		inc rsi
+		jmp .loop
+
+	.quote:
+		mov bl, 34
+		jmp .change
+
+	.end:
+		xor rax, rax
+		ret
 
 ; --- data ---
 
 section .data
-str: db "section .text", 10, "global ft_strlen", 10, "global main", 10, "", 10, "; --- ft_strlen() ---", 10, "", 10, "ft_strlen:", 10, "	xor rax, rax", 10, "	.loop:", 10, "		cmp byte [rdi], 0", 10, "		je .end", 10, "		inc rdi", 10, "		inc rax", 10, "		jmp .loop", 10, "	.end:", 10, "		ret", 10, "", 10, "; --- main() ---", 10, "", 10, "main:", 10, "	mov rdi, str", 10, "	call ft_strlen", 10, "	mov rdi, 1", 10, "	mov rsi, str", 10, "	mov edx, eax", 10, "	mov rax, 1", 10, "	syscall", 10, "	xor rax, rax", 10, "	ret", 10, "", 10, "; --- data ---", 10, "", 10, "section .data", 10, "str: db", 10, "", 10, "section .note.GNU-stack noalloc noexec nowrite align=1", 10, 0
+
+str: db "section .text\nglobal main\n\n; --- ft_putstr ---\n\nft_putstr:\n\tmov rax, 1\n\tmov rsi, rdi\n\tmov rdi, 1\n\tmov rdx, len - 1\n\tsyscall\n\tret\n\n; --- main ---\n\nmain:\n\tmov rdx, 1\n\tmov rsi, str\n\tmov rdi, 1\n\n\t.loop:\n\t\tcmp byte [rsi], 0\n\t\tje .end\n\t\tcmp byte [rsi], 92\n\t\tjne .write\n\t\tinc rsi\n\t\tcmp byte [rsi], 110\n\t\tje .newline\n\t\tcmp byte [rsi], 116\n\t\tje .tab\n\t\tcmp byte [rsi], 115\n\t\tje .str\n\t\tcmp byte [rsi], 113\n\t\tje .quote\n\t\t.change\n\t\t\tmov byte [rsi], bl\n\t\t.write:\n\t\t\tmov rax, 1\n\t\t\tsyscall\n\t\t\tinc rsi\n\t\t\tjmp .loop\n\n\t.newline:\n\t\tmov bl, 10\n\t\tjmp .change\n\n\t.tab:\n\t\tmov bl, 9\n\t\tjmp .change\n\n\t.str:\n\t\tmov rbx, rsi\n\t\tmov rdi, copy\n\t\tcall ft_putstr\n\t\tmov rdx, 1\n\t\tmov rsi, rbx\n\t\tinc rsi\n\t\tjmp .loop\n\n\t.quote:\n\t\tmov bl, 34\n\t\tjmp .change\n\n\t.end:\n\t\txor rax, rax\n\t\tret\n\n; --- data ---\n\nsection .data\n\nstr: db \q\s\q, 0\ncopy: db \q\s\q, 0\nlen: equ $ - copy\n\nsection .note.GNU-stack noalloc noexec nowrite align=1\n", 0
+copy: db "section .text\nglobal main\n\n; --- ft_putstr ---\n\nft_putstr:\n\tmov rax, 1\n\tmov rsi, rdi\n\tmov rdi, 1\n\tmov rdx, len - 1\n\tsyscall\n\tret\n\n; --- main ---\n\nmain:\n\tmov rdx, 1\n\tmov rsi, str\n\tmov rdi, 1\n\n\t.loop:\n\t\tcmp byte [rsi], 0\n\t\tje .end\n\t\tcmp byte [rsi], 92\n\t\tjne .write\n\t\tinc rsi\n\t\tcmp byte [rsi], 110\n\t\tje .newline\n\t\tcmp byte [rsi], 116\n\t\tje .tab\n\t\tcmp byte [rsi], 115\n\t\tje .str\n\t\tcmp byte [rsi], 113\n\t\tje .quote\n\t\t.change\n\t\t\tmov byte [rsi], bl\n\t\t.write:\n\t\t\tmov rax, 1\n\t\t\tsyscall\n\t\t\tinc rsi\n\t\t\tjmp .loop\n\n\t.newline:\n\t\tmov bl, 10\n\t\tjmp .change\n\n\t.tab:\n\t\tmov bl, 9\n\t\tjmp .change\n\n\t.str:\n\t\tmov rbx, rsi\n\t\tmov rdi, copy\n\t\tcall ft_putstr\n\t\tmov rdx, 1\n\t\tmov rsi, rbx\n\t\tinc rsi\n\t\tjmp .loop\n\n\t.quote:\n\t\tmov bl, 34\n\t\tjmp .change\n\n\t.end:\n\t\txor rax, rax\n\t\tret\n\n; --- data ---\n\nsection .data\n\nstr: db \q\s\q, 0\ncopy: db \q\s\q, 0\nlen: equ $ - copy\n\nsection .note.GNU-stack noalloc noexec nowrite align=1\n", 0
+len: equ $ - copy
 
 section .note.GNU-stack noalloc noexec nowrite align=1
