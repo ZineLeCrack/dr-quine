0a410a91d81b92a76932cda914465d9b3b1bf83a
ZineLeCrack <rlebaillif27@gmail.com>
2025-10-15 17:00:01 +0200

    2/C

---
+++ 2/C/Grace.c
@@ -0,0 +1,7 @@
+// Grace
+#include <stdio.h>
+#include <fcntl.h>
+#define STR "// Grace%c#include <stdio.h>%c#include <fcntl.h>%c#define STR %c%s%c%c#define PRINT(fd) dprintf(fd, STR, 10, 10, 10, 34, STR, 34, 10, 10, 34, 34, 10, 10)%c#define RUN int main() { int fd = open(%cGrace_kid.c%c, O_CREAT | O_WRONLY, 0664); PRINT(fd); }%cRUN%c"
+#define PRINT(fd) dprintf(fd, STR, 10, 10, 10, 34, STR, 34, 10, 10, 34, 34, 10, 10)
+#define RUN int main() { int fd = open("Grace_kid.c", O_CREAT | O_WRONLY, 0664); PRINT(fd); }
+RUN
