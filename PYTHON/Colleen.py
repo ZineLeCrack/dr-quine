f8dff06237386f6f7ac8f86c20256527ed8f5a5b
ZineLeCrack <rlebaillif27@gmail.com>
2025-10-19 16:08:10 +0200

    python colleen now 2 comments

--- PYTHON/Colleen.py
+++ PYTHON/Colleen.py
@@ -1,6 +1,7 @@
 # python
 
 def ft_printf(str):
+	# main loop
 	for i in range(len(str)):
 		if i != 0 and str[i - 1] == '!' and (str[i] == 'n' or str[i] == 't' or str[i] == 's'):
 			continue
@@ -16,5 +17,5 @@ def ft_printf(str):
 		else:
 			print(str[i], end='')
 
-str = "# python!n!ndef ft_printf(str):!n!tfor i in range(len(str)):!n!t!tif i != 0 and str[i - 1] == '!' and (str[i] == 'n' or str[i] == 't' or str[i] == 's'):!n!t!t!tcontinue!n!t!tif str[i] == '!':!n!t!t!tif str[i + 1] == 'n':!n!t!t!t!tprint(chr(10), end='')!n!t!t!telif str[i + 1] == 't':!n!t!t!t!tprint(chr(9), end='')!n!t!t!telif str[i + 1] == 's':!n!t!t!t!tprint(chr(34) + str + chr(34), end='')!n!t!t!telse:!n!t!t!t!tprint(str[i], end='')!n!t!telse:!n!t!t!tprint(str[i], end='')!n!nstr = !s!nft_printf(str)!n"
+str = "# python!n!ndef ft_printf(str):!n!t# main loop!n!tfor i in range(len(str)):!n!t!tif i != 0 and str[i - 1] == '!' and (str[i] == 'n' or str[i] == 't' or str[i] == 's'):!n!t!t!tcontinue!n!t!tif str[i] == '!':!n!t!t!tif str[i + 1] == 'n':!n!t!t!t!tprint(chr(10), end='')!n!t!t!telif str[i + 1] == 't':!n!t!t!t!tprint(chr(9), end='')!n!t!t!telif str[i + 1] == 's':!n!t!t!t!tprint(chr(34) + str + chr(34), end='')!n!t!t!telse:!n!t!t!t!tprint(str[i], end='')!n!t!telse:!n!t!t!tprint(str[i], end='')!n!nstr = !s!nft_printf(str)!n"
 ft_printf(str)
