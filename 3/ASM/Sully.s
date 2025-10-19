24398c2ad31dc8766058706bc05fdbfd3ddea445
ZineLeCrack <rlebaillif27@gmail.com>
2025-10-19 12:35:56 +0200

    3/ASM OK with Sully_5

--- 3/ASM/Sully.s
+++ 3/ASM/Sully.s
@@ -35,11 +35,18 @@ main:
 	cmp r8b, 0
 	jle .fail
 	add r8b, 47
-	mov byte [filename + 6], r8b
-	mov byte [cmd + 34], r8b
-	mov byte [cmd + 81], r8b
-	OPEN_FILE
-	mov rbx, str
+	mov rax, 21
+	mov rdi, path
+	mov rsi, 4
+	syscall
+	cmp rax, 0
+	jl .no_access
+	.start:
+		mov byte [filename + 6], r8b
+		mov byte [cmd + 34], r8b
+		mov byte [cmd + 81], r8b
+		OPEN_FILE
+		mov rbx, str
 	.loop:
 		cmp byte [rbx], 0
 		je .end
@@ -70,6 +77,10 @@ main:
 	.fail:
 		ret
 
+	.no_access:
+		inc r8b
+		jmp .start
+
 	.newline:
 		mov cl, 10
 		jmp .write
@@ -92,9 +103,10 @@ main:
 		jmp .write
 
 section .data
+path: db "Sully_5.s", 0
 cmd: db "/bin/bash -c ", 34, "nasm -f elf64 Sully_X.s && gcc -Wall -Wextra -Werror -no-pie Sully_X.o -o Sully && ./Sully", 34, 0
 filename: db "Sully_X.s", 0
-str: db "section .text\nglobal main\nextern system\n\n%macro PUTCHAR 1\n\tmov byte [tmp], %1\n\tmov rax, 1\n\tmov rdi, r9\n\tlea rsi, [tmp]\n\tmov rdx, 1\n\tsyscall\n%endmacro\n\n%macro OPEN_FILE 0\n\tmov rax, 2\n\tmov rdi, filename\n\tmov rsi, 0102o\n\tmov rdx, 0644o\n\tsyscall\n\tcmp rax, 0\n\tjl .fail\n\tmov r9, rax\n%endmacro\n\n%macro PUTSTR 0\n\tmov rax, 1\n\tmov rdi, r9\n\tmov rsi, str\n\tmov rdx, len - 1\n\tsyscall\n%endmacro\n\nmain:\n\tmov r8b, \x\n\tcmp r8b, 0\n\tjle .fail\n\tadd r8b, 47\n\tmov byte [filename + 6], r8b\n\tmov byte [cmd + 34], r8b\n\tmov byte [cmd + 81], r8b\n\tOPEN_FILE\n\tmov rbx, str\n\t.loop:\n\t\tcmp byte [rbx], 0\n\t\tje .end\n\t\tcmp byte [rbx], 92\n\t\tjne .normal\n\t\tinc rbx\n\t\tcmp byte [rbx], 110\n\t\tje .newline\n\t\tcmp byte [rbx], 116\n\t\tje .tab\n\t\tcmp byte [rbx], 113\n\t\tje .quote\n\t\tcmp byte [rbx], 115\n\t\tje .str\n\t\tcmp byte [rbx], 120\n\t\tje .number\n\t.normal:\n\t\tmov cl, byte [rbx]\n\t.write:\n\t\tPUTCHAR cl\n\t\tinc rbx\n\t\tjmp .loop\n\t.end:\n\t\tlea rdi, [rel cmd]\n\t\tsub rsp, 8\n\t\tcall system\n\t\tadd rsp, 8\n\t.fail:\n\t\tret\n\n\t.newline:\n\t\tmov cl, 10\n\t\tjmp .write\n\n\t.tab:\n\t\tmov cl, 9\n\t\tjmp .write\n\n\t.quote:\n\t\tmov cl, 34\n\t\tjmp .write\n\n\t.str:\n\t\tPUTSTR\n\t\tinc rbx\n\t\tjmp .loop\n\n\t.number:\n\t\tmov cl, r8b\n\t\tjmp .write\n\nsection .data\ncmd: db \q/bin/bash -c \q, 34, \qnasm -f elf64 Sully_X.s && gcc -Wall -Wextra -Werror -no-pie Sully_X.o -o Sully && ./Sully\q, 34, 0\nfilename: db \qSully_X.s\q, 0\nstr: db \q\s\q, 0\nlen: equ $ - str\n\nsection .bss\ntmp resb 1\n\nsection .note.GNU-stack noalloc noexec nowrite align=1\n", 0
+str: db "section .text\nglobal main\nextern system\n\n%macro PUTCHAR 1\n\tmov byte [tmp], %1\n\tmov rax, 1\n\tmov rdi, r9\n\tlea rsi, [tmp]\n\tmov rdx, 1\n\tsyscall\n%endmacro\n\n%macro OPEN_FILE 0\n\tmov rax, 2\n\tmov rdi, filename\n\tmov rsi, 0102o\n\tmov rdx, 0644o\n\tsyscall\n\tcmp rax, 0\n\tjl .fail\n\tmov r9, rax\n%endmacro\n\n%macro PUTSTR 0\n\tmov rax, 1\n\tmov rdi, r9\n\tmov rsi, str\n\tmov rdx, len - 1\n\tsyscall\n%endmacro\n\nmain:\n\tmov r8b, \x\n\tcmp r8b, 0\n\tjle .fail\n\tadd r8b, 47\n\tmov rax, 21\n\tmov rdi, path\n\tmov rsi, 4\n\tsyscall\n\tcmp rax, 0\n\tjl .no_access\n\t.start:\n\t\tmov byte [filename + 6], r8b\n\t\tmov byte [cmd + 34], r8b\n\t\tmov byte [cmd + 81], r8b\n\t\tOPEN_FILE\n\t\tmov rbx, str\n\t.loop:\n\t\tcmp byte [rbx], 0\n\t\tje .end\n\t\tcmp byte [rbx], 92\n\t\tjne .normal\n\t\tinc rbx\n\t\tcmp byte [rbx], 110\n\t\tje .newline\n\t\tcmp byte [rbx], 116\n\t\tje .tab\n\t\tcmp byte [rbx], 113\n\t\tje .quote\n\t\tcmp byte [rbx], 115\n\t\tje .str\n\t\tcmp byte [rbx], 120\n\t\tje .number\n\t.normal:\n\t\tmov cl, byte [rbx]\n\t.write:\n\t\tPUTCHAR cl\n\t\tinc rbx\n\t\tjmp .loop\n\t.end:\n\t\tlea rdi, [rel cmd]\n\t\tsub rsp, 8\n\t\tcall system\n\t\tadd rsp, 8\n\t.fail:\n\t\tret\n\n\t.no_access:\n\t\tinc r8b\n\t\tjmp .start\n\n\t.newline:\n\t\tmov cl, 10\n\t\tjmp .write\n\n\t.tab:\n\t\tmov cl, 9\n\t\tjmp .write\n\n\t.quote:\n\t\tmov cl, 34\n\t\tjmp .write\n\n\t.str:\n\t\tPUTSTR\n\t\tinc rbx\n\t\tjmp .loop\n\n\t.number:\n\t\tmov cl, r8b\n\t\tjmp .write\n\nsection .data\npath: db \qSully_5.s\q, 0\ncmd: db \q/bin/bash -c \q, 34, \qnasm -f elf64 Sully_X.s && gcc -Wall -Wextra -Werror -no-pie Sully_X.o -o Sully && ./Sully\q, 34, 0\nfilename: db \qSully_X.s\q, 0\nstr: db \q\s\q, 0\nlen: equ $ - str\n\nsection .bss\ntmp resb 1\n\nsection .note.GNU-stack noalloc noexec nowrite align=1\n", 0
 len: equ $ - str
 
 section .bss
